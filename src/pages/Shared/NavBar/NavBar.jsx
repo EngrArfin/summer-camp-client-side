8b2f1841574458c0e65b108714b296938aa33780
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-08-15 19:49:06 +0600

    basic formate

---
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h2>This Is Nav BAr</h2>
+            
+        </div>
+    );
+};
+
+export default NavBar;
